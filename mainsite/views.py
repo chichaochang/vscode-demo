0deaa1922dcb17324a62e472201222bfca9c1e23
chichaochang <chichao.chang@gmail.com>
2021-02-06 22:07:51 +0800

    add template

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 from .models import Post
 
 # Create your views here.
 
 def homepage(request):
     posts = Post.objects.all()
-    post_list = list()
-    for count, post in enumerate(posts):
-        post_list.append("No.{}:".format(str(count)) + str(post)+"<hr>")
-        post_list.append("<small>" + str(post.body) + "</small><br><br>")
-    return HttpResponse(post_list)
+    now = datetime.now()
+    return render(request, 'index.html', locals())
+    #post_list = list()
+    #for count, post in enumerate(posts):
+    #    post_list.append("No.{}:".format(str(count)) + str(post)+"<hr>")
+    #    post_list.append("<small>" + str(post.body) + "</small><br><br>")
+    #return HttpResponse(post_list)
